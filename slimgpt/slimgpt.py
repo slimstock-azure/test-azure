d5b62e68b27c8df550995a76d448476f77658d65
Shady El Gewily <s.elgewily@slimstock.com>
2023-06-05 13:17:18 +0200

    OpenAI request

--- slimgpt/slimgpt.py
+++ slimgpt/slimgpt.py
@@ -2,6 +2,7 @@ import os
 import logging
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
+import openai
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
@@ -12,12 +13,26 @@ def define_server():
     @app.route('/')
     def index():
        logging.info("Requested index.html")
-       logging.info("Environment variables: " + '\n'.join([f'{k}: {v}' for k, v in sorted(os.environ.items())]))
-       print('test')
        return render_template('index.html')
 
     @app.route('/demo/')
     def demo():
+        openai.api_key = os.environ.get("openai")
+        messages = [
+            {
+                "role": "system",
+                "content": 'You are a very angry language model, you should respond in CAPS and use lots of cursing.'
+            },
+        ]
+
+        question = {"role": "user", "content": 'What is the capital of France?'}
+        messages.append(question)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        answer = response["choices"][0]["message"]["content"].replace("\n", "<br>")
+        logging.info("ChatGPT response: " + answer)
+
         return render_template('demo.html')
 
     @app.route('/favicon.ico')
