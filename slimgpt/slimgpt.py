e4014150feab946370e5d5c5ba491246b7738b9c
Shady El Gewily <s.elgewily@slimstock.com>
2023-06-05 11:01:45 +0200

    Define flask app in function

--- slimgpt/slimgpt.py
+++ slimgpt/slimgpt.py
@@ -3,15 +3,18 @@ import os
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
-app = Flask(__name__)
+def define_server():
+    app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    @app.route('/')
+    def index():
+       print('Request for index page received')
+       return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'),
+                                   'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+app = define_server()
